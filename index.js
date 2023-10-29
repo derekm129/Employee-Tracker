eedaa1b0bc3c40f4ad66ce255688482bf77532d0
dmod129 <derek.modugno@gmail.com>
2023-10-29 15:08:50 -0500

    created prompt

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const inquirer = require('inquirer');
 const mysql = require('mysql2');
 const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
+    host: 'localhost',
+    user: 'root',
     password: 'password1234',
     database: 'employees_db',
 });
@@ -14,3 +14,18 @@ db.connect(err => {
     }
     console.log("MySQL2 Connected");
   });
+
+  const init = () => {
+    inquirer.prompt([
+        {
+            name: 'options',
+            type: 'list',
+            message: 'Please choose from the following options:',
+            choices: ['view all departments', 'view all roles', 'view all employees', 'add a department', 'add a role', 'add an employee', 'update an employee role']
+
+
+        }
+    ]);
+  };
+
+  init();
