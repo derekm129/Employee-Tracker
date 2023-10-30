41320f13656d654968450393cd81375de72a9d68
dmod129 <derek.modugno@gmail.com>
2023-10-30 17:21:48 -0500

    added view functions

--- index.js
+++ index.js
@@ -13,19 +13,69 @@ db.connect(err => {
       throw err;
     }
     console.log("MySQL2 Connected");
+    init();
   });
 
+//   Initial prompt
   const init = () => {
     inquirer.prompt([
         {
             name: 'options',
             type: 'list',
             message: 'Please choose from the following options:',
-            choices: ['view all departments', 'view all roles', 'view all employees', 'add a department', 'add a role', 'add an employee', 'update an employee role']
-
-
+            choices: [
+                'View all departments', 
+                'View all roles', 
+                'View all employees', 
+                'Add a department', 
+                'Add a role', 
+                'Add an employee', 
+                'Update an employee role']
         }
-    ]);
+    ]).then(answer => {
+        switch  (answer.options) {
+            case 'View all departments':
+                viewDepartments();
+                break;
+            case 'View all roles':
+                viewRoles();
+                break;
+            case 'View all employees':
+                viewEmployees();
+                break;  
+        }
+    });
   };
 
-  init();
+const viewDepartments = () => {
+    const query = 'SELECT DISTINCT * FROM departments';
+    db.query(query, (err, results) => {
+        if (err) {
+            throw err;
+        }
+        console.table(results);
+        init();
+    });
+};
+
+const viewRoles = () => {
+    const query = 'SELECT DISTINCT * FROM roles';
+    db.query(query, (err, results) => {
+        if (err) {
+            throw err;
+        }
+        console.table(results);
+        init();
+    });
+};
+
+const viewEmployees = () => {
+    const query = 'SELECT DISTINCT * FROM employees';
+    db.query(query, (err, results) => {
+        if (err) {
+            throw err;
+        }
+        console.table(results);
+        init();
+    });
+};
